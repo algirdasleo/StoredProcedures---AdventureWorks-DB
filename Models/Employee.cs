121babdb9c21acc8fc9a5b236fb710f70f1216ba
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-05-20 16:26:19 +0300

    Set strings to empty and renamed employeemodel to employee

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,33 +1,33 @@
 namespace API.Models
 {
-    public class EmployeeModel
+    public class Employee
     {
         // Employee information
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public char Gender { get; set }
-        public string NationalIDNumber { get; set; }
-        public string JobTitle { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public char? Gender { get; set; }
+        public string NationalIDNumber { get; set; } = string.Empty;
+        public string JobTitle { get; set; } = string.Empty;
 
         // Department information
         public int DepartmentID { get; set; }
-        public string DepartmentName { get; set; }
-        public string DepartmentGroup { get; set; }
+        public string DepartmentName { get; set; } = string.Empty;
+        public string DepartmentGroup { get; set; } = string.Empty;
 
         // Dates
         public DateTime HireDate { get; set; }
         public DateTime BirthDate { get; set; }
 
         // Contact information
-        public string EmailAddress { get; set; }
-        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
 
         // Address information
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
-        public string City { get; set; }
+        public string AddressLine1 { get; set; } = string.Empty; 
+        public string AddressLine2 { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
         public int StateProvinceID { get; set; }
-        public string PostalCode { get; set; }
+        public string PostalCode { get; set; } = string.Empty;
     }
 }
