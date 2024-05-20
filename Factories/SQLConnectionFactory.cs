5a2ad1ad264945f267ba8682339ae07e72c809f7
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-05-20 15:30:31 +0300

    Added connection factory for easy DB access

---
+++ Factories/SQLConnectionFactory.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace API.Factories
+{
+    public class SqlConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public IDbConnection CreateConnection()
+        {
+            var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
